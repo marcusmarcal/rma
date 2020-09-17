8ddb4b68536f17466c4964ea0473a5ec72121f3e
Marcus Marçal <marcus.marcal@gmail.com>
2020-09-17 18:49:23 -0300

    Atualizando DB

--- www/conexao.php
+++ www/conexao.php
@@ -1,8 +1,11 @@
-<?
-$mysql_server = "url_do_servidor";
-$mysql_user = "usuário";
-$mysql_pass = "senha";
-$mysql_db = "nome_do_banco";
-$conexao = mysql_connect($mysql_server, $mysql_user, $mysql_pass) or die("N&atilde;o foi possivel conectar ao banco!!" . $mysql_error);
+<?php
+
+$mysql_server = "rma-mysql";
+$mysql_user = "root";
+$mysql_pass = "uSD97a!@12?";
+$mysql_db = "rma";
+
+
+$conexao = mysqli_connect($mysql_server, $mysql_user, $mysql_pass) or die("N&atilde;o foi possivel conectar ao banco!!" . $mysql_error);
+
 $banco = mysql_select_db($mysql_db, $conexao);
-?>
